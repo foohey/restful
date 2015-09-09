acf236c30bfc89d3df8370e38547209e4f22a47c
Mathieu Robardey <mathieu@altagem.com>
2015-09-09 19:48:09 +0200

    STEP 4 - Search users
    GET /search/users?q=foobar

---
+++ api/search_users.rb
@@ -0,0 +1,23 @@
+module Restful
+  class SearchUsers < Grape::API
+    get '/search/users' do
+      search_term = params[ :q ].downcase
+      user_table  = $DB[:user]
+      columns     = ( user_table.columns - [:password] ) # Remove password from query
+
+      results = user_table
+        .select( *columns )
+        .where(
+          Sequel.like( :email,     "%#{search_term}%" ) |
+          Sequel.like( :firstname, "%#{search_term}%" ) |
+          Sequel.like( :lastname,  "%#{search_term}%" )
+        )
+
+      unless results.empty?
+        return results.to_a
+      else
+        return error!( { status: 404, message: 'not found' }, 404 )
+      end
+    end
+  end
+end
